5d77bd63f0b34d49b544c68af6dae66d75b68641
musajama <musanuh36@gmail.com>
2025-04-09 20:21:59 -0400

    Use destruction for object and array

--- script.js
+++ script.js
@@ -18,3 +18,12 @@ const student = {
   const newStudent = JSON.parse(studentJSON);
   console.log("Parsed Object:", newStudent);
   console.log("Are objects equal?", JSON.stringify(student) === JSON.stringify(newStudent));
+
+  const { name, courses } = student;
+  console.log("Destructured Name:", name);
+  console.log("Destructured Courses:", courses);
+ 
+  const scores = [85, 92, 78, 90];
+  const [score1, score2] = scores;
+  console.log("First Score:", score1);
+  console.log("Second Score:", score2);
