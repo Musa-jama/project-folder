3d0359feec426656cc229e73aa5c0fc99b9dd3d4
musajama <musanuh36@gmail.com>
2025-04-09 20:14:05 -0400

    Initial commit with script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const student = {
+    name: "Henry Thierry",
+    age: 40,
+    enrolled: true,
+    courses: ["Javascript", "Html", "Styles.css"],
+    displayInfo: function () {
+      return `Student: ${this.name}, Age: ${this.age}, Enrolled: ${this.enrolled}`;
+    }
+  };
+ 
